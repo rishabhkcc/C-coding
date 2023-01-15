4354e889f7cd3585cd1c2913f318cdf7d1816dc9
Rishabh rao <107990966+rrao2521@users.noreply.github.com>
2023-01-15 23:37:15 +0530

    Create jewels_and_stone.cpp

---
+++ jewels_and_stone.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+    int numJewelsInStones(string j, string s) {
+        int count = 0;
+        vector<int> hash(256,0);
+        for(auto a:s)
+            hash[a]++;
+        for(auto b:j){
+            count += hash[b];
+        }
+        return count;
+    }
+};
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Secur
