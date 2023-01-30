13f2eac8dba412658a8ff97fadff1500a4cdca29
Rishabh rao <107990966+rrao2521@users.noreply.github.com>
2023-01-30 21:30:02 +0530

    Create Basic_data_type.cpp

---
+++ Basic_data_type.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    cout << x << endl;
+    long w;
+    cin >> w;
+    cout << w << endl;
+    char alpha;
+    cin >> alpha;
+    cout << alpha << endl;
+    float y;
+    cin >> y;
+    cout << setprecision(7) << y << endl;
+    double z;
+    cin >> z;
+    cout << setprecision(15)<< z << endl;
+    return 0;
+}
