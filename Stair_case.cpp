e657aa9acd6f1ef206193f0181a418bf882ed8c0
Rishabh rao <107990966+rrao2521@users.noreply.github.com>
2023-01-31 15:29:04 +0530

    Create Stair_case.cpp

---
+++ Stair_case.cpp
@@ -0,0 +1,13 @@
+void staircase(int n) {
+    for(int i=n;i>0;i--){
+        for(int j=1;j<=n;j++){
+            if(j>=i){
+                cout<<"#";
+            }else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+        
+    }
+}
