61a2f0a2590db467b072de58cf2667977a7e5134
Rishabh rao <107990966+rrao2521@users.noreply.github.com>
2023-02-01 15:22:21 +0530

    Create Print_the_elements_of_a_linkedlist.cpp

---
+++ Print_the_elements_of_a_linkedlist.cpp
@@ -0,0 +1,6 @@
+void printLinkedList(SinglyLinkedListNode* head) {
+    SinglyLinkedListNode* temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<endl;
+        temp = temp ->next;
+    }
