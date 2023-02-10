8abc6f5d6041f161f313c773ad2bbf486fbb15b6
Rishabh rao <107990966+rrao2521@users.noreply.github.com>
2023-02-10 19:44:21 +0530

    Create Pre_order_binary_tree.cpp

---
+++ Pre_order_binary_tree.cpp
@@ -0,0 +1,8 @@
+void preOrder(Node *root) {
+        if(root != NULL){
+            cout<<root->data<<" ";
+            preOrder(root->left);
+            preOrder(root->right);
+        }
+        
+    }
