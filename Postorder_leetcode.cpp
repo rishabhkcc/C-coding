b77efd21a7f4be15095aa4f291e235da2a66f7fc
Rishabh rao <107990966+rrao2521@users.noreply.github.com>
2023-02-10 19:53:56 +0530

    Create Postorder_leetcode.cpp

---
+++ Postorder_leetcode.cpp
@@ -0,0 +1,15 @@
+class Solution {
+private:
+    void solve(TreeNode* root, vector<int> &ans){
+        if(root == NULL) return ;
+        solve(root->left, ans);
+        solve(root->right, ans);
+        ans.push_back(root->val);
+    }
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        solve(root, ans);
+        return ans;
+    }
+};
